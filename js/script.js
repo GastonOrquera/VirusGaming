a92a562bc59f3a791ea4b467fa281924894b0189
GastonOrquera <gastonorquera29@gmail.com>
2023-07-30 01:28:36 -0300

    VirusGaming

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
